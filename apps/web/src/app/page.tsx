90782c69fa8707998aae6426ee9d5e77063f4fd7
Belsetbut <belstimothee@gmail.com>
2025-12-26 23:10:09 +0100

    make header butiful

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { Header } from "@/components/header";
+
 export default function Home() {
 
   return (
     <div>
-      
+      <Header />
     </div>
   );
 }
--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,32 +1,38 @@
-"use client";
-import Link from "next/link";
+"use client"
 
-import { ModeToggle } from "./mode-toggle";
+import type { Route } from 'next';
+import { Button } from './ui/button';
+import Link from 'next/link';
+import { ArrowDown, MousePointerClickIcon } from 'lucide-react';
 
-export default function Header() {
-  const links = [
-    { to: "/", label: "Home" },
-    { to: "/dashboard", label: "Dashboard" },
-    { to: "/todos", label: "Todos" },
-  ] as const;
+export function Header() {
 
   return (
+  <section
+   id="home"
+   className="pt-24 pb-16 min-h-screen flex items-center relative bg-center bg-fill"
+   style={{
+     backgroundImage:
+       "url('/bg.png')",
+   }}>
     <div>
-      <div className="flex flex-row items-center justify-between px-2 py-1">
-        <nav className="flex gap-4 text-lg">
-          {links.map(({ to, label }) => {
-            return (
-              <Link key={to} href={to}>
-                {label}
-              </Link>
-            );
-          })}
-        </nav>
-        <div className="flex items-center gap-2">
-          <ModeToggle />
+      <div>
+        <h1 className="fixed text-7xl font-bold left-1/2 -translate-x-1/2 top-15">IGG TECHNIK</h1>
+        <div className='fixed bottom-1/2 left-1/2 -translate-x-1/2'>
+          <Link href={"/sign-in" as Route}>
+        <Button className="rounded text-4xl border-25 border-gray-100 shadow-md hover:cursor-pointer hover:shadow-xl text-black">
+          Anmelden
+        </Button>
+         </Link>
+        </div>
+        <div className='fixed bottom-1/4 left-1/2 -translate-x-1/2'>
+        <Link href={"#calendar"}>
+         <p className='text-5xl border-gray-200 bg-gray-200 rounded-xl border-10'>Kalender</p>
+         <ArrowDown size={100} className='fixed translate-x-1/2 mt-10'/>
+        </Link>
         </div>
       </div>
-      <hr />
     </div>
-  );
+  </section>
+  )
 }
