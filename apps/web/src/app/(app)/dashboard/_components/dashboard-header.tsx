b739ac9cdeb8e4f1409891fbcd33c89c8cac603d
Elias <contact@eliasnau.dev>
2026-02-18 20:07:39 +0100

    fix: navbar and a small typo
    Co-authored-by: Belset But <belstimothee@gmail.com>"

--- apps/web/src/app/(app)/dashboard/_components/dashboard-header.tsx
+++ apps/web/src/app/(app)/dashboard/_components/dashboard-header.tsx
@@ -7,7 +7,7 @@ import { DynamicBreadcrumbs } from "./dynamic-breadcrumbs";
 
 export function DashboardHeader() {
 	return (
-		<header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+		<header className="z-20 flex h-16 shrink-0 items-center gap-2 border-[#222] border-b bg-[#0a0a0a] transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
 			<div className="flex items-center gap-2 px-4">
 				<SidebarTrigger onClick={haptic} className="-ml-1 hidden md:flex" />
 				<Separator
